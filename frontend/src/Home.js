d1aab6fe53602d99afb17e4f4e82a6594ab963b8
Gregório A. Almeida Filho <gregassagraf@usp.br>
2023-06-18 18:54:34 -0300

    Modificações: 1. Ajustes no layout

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Masonry from 'masonry-layout';
+import imagesLoaded from 'imagesloaded';
 import SearchBox from './components/searchBox';
 import axios from "axios";
+
 export default class Home extends Component {
     constructor() {
         super();
         this.state = {
             videos: []
         };
+        this.gridRef = React.createRef();
     }
 
     componentDidMount() {
         this.fetchVideos();
     }
+
+    componentDidUpdate() {
+        // Inicializa o Masonry
+        const grid = this.gridRef.current;
+        const gridWidth = 1350;
+        const numberOfColumns = 6;
+        const columnWidth = gridWidth / numberOfColumns;
+        imagesLoaded(grid, () => {
+            new Masonry(grid, {
+                itemSelector: '.grid-item',
+                columnWidth: columnWidth,
+                percentPosition: true,
+            });
+        });
+    }
+
     async fetchVideos() {
         try {
             const response = await fetch('http://localhost:4000/videos');
@@ -42,21 +62,23 @@ export default class Home extends Component {
         const { videos } = this.state;
         return (
             <div className="App App-header">
-                <div className="container">
-                    <div className="row">
+                <h1 style={{fontSize: '30px', marginTop: '25px', marginBottom: '25px'}}>30 TECNOLOGIAS DA USP QUE VOCÊ PRECISA CONHECER!</h1>
+                <div className="container" style={{maxWidth: '1350px'}}>
+                    <div className="grid" ref={this.gridRef}>
+                        <div className="grid-sizer"></div>
                         {videos.map(video => (
-                            <div className="col-md-4" key={video.id}>
+                            <div className="grid-item" key={video.id} style={{ maxWidth: '180px', marginBottom: '35px' }}>
                                 <Link to={`/player/${video.id}`}>
                                     <div className="card border-0">
                                         <div className="card-body">
-                                            <p>{video.title}</p>
+                                            <p style={{fontSize: '12px'}}>{video.title}</p>
                                         </div>
                                     </div>
                                 </Link>
                             </div>
                         ))}
                     </div>
-                    <div className="searchBox">
+                    <div className="searchBox" style={{position: 'absolute', right: '50px', top: '50px', fontSize: '15px'}}>
                         <SearchBox callback={this.handleSearch} />
                     </div>
                 </div>
